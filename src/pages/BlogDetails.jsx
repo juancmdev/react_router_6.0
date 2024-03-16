2c4154813489670cfd7f055cc1919b4099c9cd01
DraxusTDEV <draxustienda@gmail.com>
2024-03-15 21:35:12 -0500

    useParams implementado

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,5 +1,26 @@
+import { Link, useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
 const BlogDetails = () => {
-  return "BlogDetails";
+  const params = useParams();
+  //console.log(params.id);
+
+  const { data, loading, error } = useFetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+
+  if (loading) return <p>Loading data............</p>;
+  if (error) return <p>error.....................</p>;
+
+  return (
+    <>
+      <h1>
+        {data.id} - {data.title}
+      </h1>
+      <p>{data.body}</p>
+      <Link to={"/blog"}>Volver</Link>
+    </>
+  );
 };
 
 export default BlogDetails;
